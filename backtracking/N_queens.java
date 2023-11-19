d8e6917f168ba61fb69e113479052c1acf1f4dfc
Rejona <rejwork0403@gmail.com>
2023-11-19 22:52:25 +0530

    w

--- backtracking/N_queens.java
+++ backtracking/N_queens.java
@@ -2,13 +2,30 @@ import java.util.*;
 
 public class N_queens {
 
+    public 
+
     public void helper(char[][] board, List<List<String>> allboards, int col){
         
+        if(col = board.length){
+            saveboards(board, allboards);
+            return;
+        }
+
+        for(int row = 0; row<board.length; row++){
+            if(isSafe(row, col, board)){
+                board[row][col] = 'Q';
+                helper(board, allboards, col+1);
+                board[row][col] = '.';
+            }
+        }
     }
 
     public List<List<String>> solveQueen(int n){
 
+        List<List<String>> allboards = new ArrayList<>();
         char [][] board = new char[n][n];
+        helper(board, allboards, 0);
+        return allboards;
     }
     
 }
